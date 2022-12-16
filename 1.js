37fa64cc90a26e7c792b30b3e5952be4b2618ca3
Yaroslav Myratov <yaroslav.muratov.97@inbox.ru>
2022-12-16 08:46:03 +0300

    privtik

---
+++ 1.js
@@ -0,0 +1,14 @@
+function multiplyAll(arr) {
+    let product = 1;
+    // Only change code below this line
+    for (let i = 0; i < arr.length; i++) {
+        const subArray = arr[i];
+        for (let j = 0; j < subArray.length; j++) {
+            product *= subArray[j];
+        }
+    }
+    // Only change code above this line
+    return product;
+}
+
+multiplyAll([[1,2],[3,4],[5,6,7]]);
